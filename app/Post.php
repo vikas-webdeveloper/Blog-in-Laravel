ac01dbb2c7ee0a40f9b17f69179f19c4285f3e28
Vikas Rana <vikas.webdeveloper@yahoo.com>
2016-08-20 13:46:50 +0530

    Seeding Posts with test data

--- app/Post.php
+++ app/Post.php
@@ -6,16 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $dates=['published_at'];
+  protected $dates = ['published_at'];
 
-    public function setTitleAttribute($value)
-    {
-    	$this>attributes['title'] = $value;
-    	
-    	if (! $this->exists)
-    	 {
-     	 	$this->attributes['slug'] = str_slug($value);
-    	 }
+  public function setTitleAttribute($value)
+  {
+    $this->attributes['title'] = $value;
 
+    if (! $this->exists) {
+      $this->attributes['slug'] = str_slug($value);
     }
+  }
 }
+
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,36 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run()
+  {
+   
+    $this->call('PostTableSeeder');
+  }
+
 }
+
+class PostTableSeeder extends Seeder
+{
+  public function run()
+  {
+    App\Post::truncate();
+
+    factory(App\Post::class, 20)->create();
+  }
+}
+
+/*class UserstableSeeder extends Seeder
+{
+	public function run()
+	{
+		App\User::truncate();
+		factory(App\User::class, 5)->create();
+	}
+}*/
